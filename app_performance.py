d4969b9cd2bf47844bb90bcdf879ea811b03ca03
kustex <lucaskustermans@gmail.com>
2022-06-22 17:00:10 +0200

    changed file navigator

--- app_performance.py
+++ app_performance.py
@@ -11,7 +11,7 @@ extension = 'csv'
 files = os.listdir('tickers')
 files = [i[:-4] for i in files]
 
-corr_tickers = pd.read_csv('~/Documents/Repos/risk_ranges/dashboard_streamlit/tickers_corr/correlations.csv')
+corr_tickers = pd.read_csv('../tickers_corr/correlations.csv')
 corr_tickers = list(corr_tickers['Ticker'])
 
 
