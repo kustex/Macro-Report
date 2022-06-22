fa343a7da19ceeba86b800b23da140399c24ddb8
kustex <lucaskustermans@gmail.com>
2022-06-22 16:52:22 +0200

    new version

---
+++ app_performance.py
@@ -0,0 +1,73 @@
+import dash_bootstrap_components as dbc
+import os
+import re
+
+import pandas as pd
+from dash import Dash, html, dcc
+from dash.dependencies import Input, Output, State
+from data_performance import app_performance
+
+extension = 'csv'
+files = os.listdir('tickers')
+files = [i[:-4] for i in files]
+
+corr_tickers = pd.read_csv('~/Documents/Repos/risk_ranges/dashboard_streamlit/tickers_corr/correlations.csv')
+corr_tickers = list(corr_tickers['Ticker'])
+
+
+ap = app_performance()
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.layout = html.Div([
+    html.H2(children='Performance', style={'textAlign': 'center'}),
+    dbc.Row([
+        dbc.Col([
+            dbc.Row(
+            ),
+            dcc.Dropdown(options=files,
+                         value='sectors',
+                         id='ticker_dropdown'),
+            html.Br(),
+            html.Div(id='dd_output_container')
+        ], width={"size": 6, "offset": 3}),
+    ]),
+    html.Br(),
+    html.H2(children='Correlations', style={'textAlign': 'center'}),
+    dbc.Row([
+        dbc.Col([
+            dbc.Row(
+            ),
+            dcc.Dropdown(options=corr_tickers,
+                         value='EURUSD',
+                         id='ticker_dropdown_correlations'),
+            html.Br(),
+            html.Div(id='dd_output_container_correlations')
+        ], width={"size": 6, "offset": 3})
+    ])
+])
+
+
+@app.callback(
+    Output(component_id='dd_output_container', component_property='children'),
+    Input(component_id='ticker_dropdown', component_property='value')
+)
+def dropdown_filename(value):
+    df, ticker_list = ap.get_df_all_data('~/Documents/Repos/risk_ranges/dashboard_streamlit/tickers/{}.csv'.format(value))
+    df_performance = ap.get_performance(df)
+    return dbc.Table.from_dataframe(
+        df_performance.round(2),
+        bordered=True)
+
+@app.callback(
+    Output(component_id='dd_output_container_correlations', component_property='children'),
+    Input(component_id='ticker_dropdown_correlations', component_property='value')
+)
+def dropdown_filename(value):
+    df, ticker_list = ap.get_df_all_data('~/Documents/Repos/risk_ranges/dashboard_streamlit/tickers_corr/correlations.csv')
+    df_correlation, dataframe = ap.get_correlation_table_window_x(df, value)
+    return dbc.Table.from_dataframe(
+        df_correlation,
+        bordered=True)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ data_performance.py
@@ -0,0 +1,226 @@
+import time
+import pandas as pd
+import numpy as np
+import os
+import glob
+import asyncio
+import ib_insync as ibi
+import itertools
+import datetime
+import re
+import random
+
+from datetime import timedelta, date
+from itertools import chain
+
+
+class app_performance:
+    def getTickerlists(self, filename):
+        stocks = {'name': [], 'exchange': [], 'currency': []}
+        cash = {'name': [], 'exchange': [], 'symbol': [], 'currency': []}
+        index = {'name': [], 'exchange': [], 'currency': []}
+        cont_futures = {'name': [], 'exchange': [], 'currency': []}
+        conid = {'name': [], 'exchange': [], 'contract_id': []}
+        securities = pd.read_csv(filename)
+        for security in securities.iterrows():
+            if security[1][1] == 'STK':
+                stocks['name'].append(security[1][0])
+                stocks['exchange'].append(security[1][2])
+                stocks['currency'].append(security[1][5])
+            elif security[1][1] == 'CASH':
+                cash['name'].append(security[1][0])
+                cash['exchange'].append(security[1][2])
+                cash['symbol'].append(security[1][5])
+                cash['currency'].append(security[1][7])
+            elif security[1][1] == 'IND':
+                index['name'].append(security[1][0])
+                index['exchange'].append(security[1][2])
+                index['currency'].append(security[1][5])
+            elif security[1][1] == 'FUT':
+                cont_futures['name'].append(security[1][0])
+                cont_futures['exchange'].append(security[1][2])
+                cont_futures['currency'].append(security[1][5])
+            else:
+                conid['name'].append(security[1][0])
+                conid['exchange'].append(security[1][2])
+                conid['contract_id'].append(security[1][8])
+
+        stocks_df = pd.DataFrame.from_dict(stocks)
+        index_df = pd.DataFrame.from_dict(index)
+        cont_futures_df = pd.DataFrame.from_dict(cont_futures)
+        contract_ids_df = pd.DataFrame.from_dict(conid)
+        cash_df = pd.DataFrame.from_dict(cash)
+        tickers = pd.concat([stocks_df, index_df, cont_futures_df, contract_ids_df, cash_df])['name']
+        return stocks_df, cash_df, index_df, cont_futures_df, contract_ids_df, tickers
+
+    def getContractlists(self, filename):
+        stocks, cash, index, futures, contract_ids, tickers = self.getTickerlists(filename)
+        c_stocks = [ibi.Stock(symbol=name, exchange=exchange, currency=currency) for name, exchange, currency in
+                    zip(stocks['name'], stocks['exchange'], stocks['currency'])]
+        c_cash = [ibi.Forex(pair=name, exchange=exchange, symbol=symbol, currency=currency) for
+                  name, exchange, symbol, currency in
+                  zip(cash['name'], cash['exchange'], cash['symbol'], cash['currency'])]
+        c_index = [ibi.Index(symbol=name, exchange=exchange, currency=currency) for name, exchange, currency in
+                   zip(index['name'], index['exchange'], index['currency'])]
+        c_fut = [ibi.ContFuture(symbol=name, exchange=exchange, currency=currency) for name, exchange, currency in
+                 zip(futures['name'], futures['exchange'], futures['currency'])]
+        c_conid = [ibi.Contract(localSymbol=name, conId=contractid, exchange=exchange) for name, contractid, exchange in
+                   zip(contract_ids['name'], contract_ids['contract_id'], contract_ids['exchange'])]
+        contracts_adjusted_last = list(chain(c_stocks, c_index, c_fut, c_conid))
+        contracts_midpoint = list(chain(c_cash))
+        return contracts_adjusted_last, contracts_midpoint, tickers
+
+    async def req_historical_data_async_adjusted_last(self, contract):
+        hist_data = await self.ib.reqHistoricalDataAsync(contract, '', barSizeSetting='1 day', durationStr='1 Y',
+                                                         whatToShow='ADJUSTED_LAST', useRTH=False)
+        return hist_data
+
+    async def req_historical_data_async_midpoint(self, contract):
+        hist_data = await self.ib.reqHistoricalDataAsync(contract, '', barSizeSetting='1 day', durationStr='1 Y',
+                                                         whatToShow='MIDPOINT', useRTH=False)
+        return hist_data
+
+    async def getHistoricaldata(self, filename):
+        self.ib = ibi.IB()
+        random_id = random.randint(0, 9999)
+        with await self.ib.connectAsync(host='127.0.0.1', port=7496, clientId=random_id):
+            contracts_adjusted_last, contracts_midpoint, tickers = self.getContractlists(filename)
+            all_bars_adj_last = await asyncio.gather(*[
+                self.req_historical_data_async_adjusted_last(contract)
+                for contract in contracts_adjusted_last
+            ])
+            all_bars_midpoint = await asyncio.gather(*[
+                self.req_historical_data_async_midpoint(contract)
+                for contract in contracts_midpoint
+            ])
+            all_bars = all_bars_adj_last + all_bars_midpoint
+            # self.ib.disconnect()
+            return all_bars, tickers
+
+    def get_df_all_data(self, filename):
+        all_bars_total = []
+        all_bars, tickers = asyncio.run(self.getHistoricaldata(filename))
+        tickers = list(tickers)
+        all_bars_total.extend(all_bars)
+        df = pd.DataFrame()
+        df['date'] = self.getDaterange(365)
+        df = df.set_index('date')
+        for ticker, bars in zip(tickers, all_bars_total):
+            dataframe = ibi.util.df(bars).iloc[:, [0, 4]]
+            dataframe = dataframe.set_index('date').rename(
+                columns={'close': f'{ticker}'})
+            df = df.join(dataframe)
+        return df.interpolate(), tickers
+
+    def get_performance(self, data):
+        data = data
+        tickerlist = list(data.columns)
+        tickerlist = " ".join(tickerlist)
+        tickerlist = tickerlist.split()
+        window_names = ['Ticker', 'Price', '1D', '1W', '3W', '1M', 'MTD', '3M', 'QTD', 'YTD', 'vs 52w max', 'vs 52w min']
+        df = pd.DataFrame()
+        len_week, len_3w, len_1m, len_mtd, len_3m, len_qtd, len_ytd = self.get_lengths_periods()
+        for ticker in tickerlist:
+            data_perf = data[ticker]
+            latest = data_perf[-2]
+            range = [3, len_week, len_3w, len_1m, len_mtd, len_3m, len_qtd, len_ytd]
+            results = []
+            for time in range:
+                if data_perf[-time] < latest:
+                    results.append((latest - data_perf[-time]) / latest)
+                else:
+                    results.append(-(data_perf[-time] - latest) / data_perf[-time])
+            yearly_high = data_perf.max()
+            yearly_low = data_perf.min()
+            vs_52_max = -(yearly_high - latest) / yearly_high
+            vs_52_min = (latest - yearly_low) / yearly_low
+            results.append(vs_52_max)
+            results.append(vs_52_min)
+            results = ["{:.2%}".format(y) for y in results]
+            results.insert(0, data_perf[-2].round(2))
+            df[ticker] = results
+        df = df.T.reset_index()
+        df.columns = window_names
+        return df.sort_values(by = 'Ticker')
+
+    def get_correlation_table_window_x(self, df, value):
+        window_list = [15, 30, 90, 120, 180]
+        tickerlist = list(df.columns)
+        TICKER = value
+        MULTP_TICKERS = [item for item in tickerlist if value not in item]
+
+        dataframe = pd.DataFrame()
+        for window in window_list:
+            for i in MULTP_TICKERS:
+                dataframe[f'{TICKER}_{i}_{window}'] = df[TICKER].rolling(window).corr(df[i])
+
+        day_15 = dataframe.filter(regex='15')
+        day_30 = dataframe.filter(regex='30')
+        day_90 = dataframe.filter(regex='90')
+        day_120 = dataframe.filter(regex='120')
+        day_180 = dataframe.filter(regex='180')
+
+        rolling_30d_max = [np.nanmax(dataframe[i]) for i in list(day_30.columns)]
+        rolling_30d_min = [np.nanmin(dataframe[i]) for i in list(day_30.columns)]
+
+        data = pd.DataFrame()
+        data['15D'] = np.array(day_15.iloc[-1, :])
+        data['30D'] = np.array(day_30.iloc[-1, :])
+        data['90D'] = np.array(day_90.iloc[-1, :])
+        data['120D'] = np.array(day_120.iloc[-1, :])
+        data['180D'] = np.array(day_180.iloc[-1, :])
+        data['1y 30d high'] = rolling_30d_max
+        data['1y 30d low'] = rolling_30d_min
+
+        data[f"{TICKER} vs"] = MULTP_TICKERS
+        cols = list(data.columns)
+        cols = [cols[-1]] + cols[:-1]
+        data = data[cols]
+        return data.round(2), dataframe
+
+    def getDaterange(self, numdays):
+        end_date = datetime.datetime.today()
+        delta = datetime.timedelta(days=numdays)
+        start_date = end_date - delta
+        date_list = pd.Series(pd.bdate_range(start_date, end_date))
+        return date_list
+
+    def get_lengths_periods(self):
+        end_dt = date.today()
+        start_dt_ytd = date(2022, 1, 1)
+        start_dt_qtd = date(2021, 6, 30)
+        start_dt_3m = end_dt - timedelta(weeks=12)
+        start_dt_mtd = end_dt.replace(day=1)
+        start_dt_week = end_dt - timedelta(weeks=1)
+        start_dt_3week = end_dt - timedelta(weeks=3)
+        start_dt_4week = end_dt - timedelta(weeks=4)
+        len_week, len_3w, len_4w, len_mtd, len_3m, len_qtd, len_ytd = 0, 0, 0, 0, 0, 0, 0
+        weekenddays = [5, 6]
+        for dt in self.daterange(start_dt_ytd, end_dt):
+            if dt.weekday() not in weekenddays:
+                len_ytd += 1
+        for dt in self.daterange(start_dt_qtd, end_dt):
+            if dt.weekday() not in weekenddays:
+                len_qtd += 1
+        for dt in self.daterange(start_dt_mtd, end_dt):
+            if dt.weekday() not in weekenddays:
+                len_mtd += 1
+        for dt in self.daterange(start_dt_3week, end_dt):
+            if dt.weekday() not in weekenddays:
+                len_3w += 1
+        for dt in self.daterange(start_dt_4week, end_dt):
+            if dt.weekday() not in weekenddays:
+                len_4w += 1
+        for dt in self.daterange(start_dt_week, end_dt):
+            if dt.weekday() not in weekenddays:
+                len_week += 1
+        for dt in self.daterange(start_dt_3m, end_dt):
+            if dt.weekday() not in weekenddays:
+                len_3m += 1
+        return len_week, len_3w, len_4w, len_mtd, len_3m, len_qtd, len_ytd
+
+    def daterange(self, date1, date2):
+        for n in range(int((date2 - date1).days) + 1):
+            yield date1 + timedelta(n)
+
+
