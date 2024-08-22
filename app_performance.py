b7f8229216c0998906481a7c30771f77eacc339f
Lucas Kustermans <lucaskustermans@outlook.com>
2024-08-22 16:35:06 +0200

    upgraded the codebase and layout of the dash-app

--- app_performance.py
+++ app_performance.py
@@ -1,8 +1,8 @@
 import dash_bootstrap_components as dbc
 import os
 import re
-
 import pandas as pd
+
 from dash import Dash, html, dcc
 from dash.dependencies import Input, Output, State
 from data_performance import app_performance
@@ -15,59 +15,119 @@ corr_tickers = list(corr_tickers['Ticker'])
 
 
 ap = app_performance()
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# Initialize the app with suppress_callback_exceptions=True
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
+app.title = "Macro Report"
+
+# Define the Sidebar
+sidebar = html.Div(
+    [
+        html.H2("Macro Report", className="display-4"),
+        html.Hr(),
+        #html.P("Navigation", className="lead"),
+        dbc.Nav(
+            [
+                dbc.NavLink("Performance", href="/performance", id="performance-link", active="exact"),
+                dbc.NavLink("Correlations", href="/correlations", id="correlations-link", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style={
+        "position": "fixed",
+        "top": 0,
+        "left": 0,
+        "bottom": 0,
+        "width": "16rem",
+        "padding": "2rem 1rem",
+        "background-color": "#f8f9fa",
+    },
+)
+
+# Define the Content Area
+content = html.Div(id="page-content", style={"margin-left": "18rem", "padding": "2rem 1rem"})
+
+# Define the app layout
 app.layout = html.Div([
-    html.H2(children='Performance', style={'textAlign': 'center'}),
-    dbc.Row([
-        dbc.Col([
-            dbc.Row(
-            ),
-            dcc.Dropdown(options=files,
-                         value='sectors',
-                         id='ticker_dropdown'),
-            html.Br(),
-            html.Div(id='dd_output_container')
-        ], width={"size": 6, "offset": 3}),
-    ]),
-    html.Br(),
-    html.H2(children='Correlations', style={'textAlign': 'center'}),
-    dbc.Row([
-        dbc.Col([
-            dbc.Row(
-            ),
-            dcc.Dropdown(options=corr_tickers,
-                         value='UUP',
-                         id='ticker_dropdown_correlations'),
-            html.Br(),
-            html.Div(id='dd_output_container_correlations')
-        ], width={"size": 6, "offset": 3})
-    ])
+    dcc.Location(id='url', refresh=False),
+    sidebar,
+    content
 ])
 
+# Callback to control page navigation
+@app.callback(
+    Output('page-content', 'children'),
+    [Input('url', 'pathname')]
+)
+def display_page(pathname):
+    if pathname == '/correlations':
+        return html.Div([
+            html.Br(),
+            html.H2(children='Correlations', style={'textAlign': 'center'}),
+            html.Br(),
+            dbc.Row([
+                dbc.Col([
+                    dcc.Dropdown(options=corr_tickers,
+                                 value='UUP',
+                                 id='ticker_dropdown_correlations'),
+                    html.Br(),
+                    html.Div(id='dd_output_container_correlations')
+                ], width={"size": 6, "offset": 3}),
+            ]),
+        ])
+    else:
+        return html.Div([
+            html.Br(),
+            html.H2(children='Performance', style={'textAlign': 'center'}),
+            html.Br(),
+            dbc.Row([
+                dbc.Col([
+                    dcc.Dropdown(options=files,
+                                 value='sectors',
+                                 id='ticker_dropdown'),
+                    html.Br(),
+                    html.Div(id='dd_output_container')
+                ], width={"size": 6, "offset": 3}),
+            ]),
+        ])  # Default page is Performance
 
+# Original callbacks
 @app.callback(
     Output(component_id='dd_output_container', component_property='children'),
     Input(component_id='ticker_dropdown', component_property='value')
 )
-def dropdown_filename(value):
-    # df, ticker_list = ap.get_df_all_data('tickers/{}.csv'.format(value))
-    df, ticker_list = ap.get_df_all_data_iex('tickers/{}.csv'.format(value))
+def update_performance(value):
+    df, ticker_list = ap.get_df_all_data(f'tickers/{value}.csv')
     df_performance = ap.get_performance(df)
-    return dbc.Table.from_dataframe(
-        df_performance.round(2),
-        bordered=True)
+    return dbc.Table.from_dataframe(df_performance.round(2), bordered=True)
 
 @app.callback(
     Output(component_id='dd_output_container_correlations', component_property='children'),
     Input(component_id='ticker_dropdown_correlations', component_property='value')
 )
-def dropdown_filename(value):
+def update_correlations(value):
     df, ticker_list = ap.get_df_all_data('tickers_corr/correlations.csv')
     df_correlation, dataframe = ap.get_correlation_table_window_x(df, value)
-    return dbc.Table.from_dataframe(
-        df_correlation,
-        bordered=True)
+    return dbc.Table.from_dataframe(df_correlation, bordered=True)
 
+# Highlight the active link
+@app.callback(
+    [Output(f"{link}-link", "active") for link in ["performance", "correlations"]],
+    [Input("url", "pathname")]
+)
+def toggle_active_links(pathname):
+    return [pathname == f"/{link}" for link in ["performance", "correlations"]]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+
+
+
+
+
+
+
--- data_performance.py
+++ data_performance.py
@@ -1,4 +1,5 @@
 import time
+import pendulum
 import pandas as pd
 import pandas_datareader as pdr
 import numpy as np
@@ -13,40 +14,49 @@ import random
 import pandas_datareader as pdr
 import pyEX as px
 import plotly.graph_objects as go
+
 from datetime import timedelta, date
 from itertools import chain
 
 
 class app_performance:
     def getTickerlists(self, filename):
+        '''
+        This function returns dictionaries with relevant information for each contract. These contracts are being used as input in to retrieve historical data from the 
+        Interactive Brokers API. Each type of contract (stock, future, foreign exchange, index) has different input needs. 
+        '''
         stocks = {'name': [], 'exchange': [], 'currency': []}
-        cash = {'name': [], 'exchange': [], 'symbol': [], 'currency': []}
+        cash = {'exchange': [], 'symbol': [], 'currency': [], 'name': []}
         index = {'name': [], 'exchange': [], 'currency': []}
-        cont_futures = {'name': [], 'exchange': [], 'currency': []}
+        cont_futures = {'name': [], 'exchange': [], 'currency': [], 'contract_id':[]}
         conid = {'name': [], 'exchange': [], 'contract_id': []}
         securities = pd.read_csv(filename)
-        for security in securities.iterrows():
-            if security[1][1] == 'STK':
-                stocks['name'].append(security[1][0])
-                stocks['exchange'].append(security[1][2])
-                stocks['currency'].append(security[1][5])
-            elif security[1][1] == 'CASH':
-                cash['name'].append(security[1][0])
-                cash['exchange'].append(security[1][2])
-                cash['symbol'].append(security[1][5])
-                cash['currency'].append(security[1][7])
-            elif security[1][1] == 'IND':
-                index['name'].append(security[1][0])
-                index['exchange'].append(security[1][2])
-                index['currency'].append(security[1][5])
-            elif security[1][1] == 'FUT':
-                cont_futures['name'].append(security[1][0])
-                cont_futures['exchange'].append(security[1][2])
-                cont_futures['currency'].append(security[1][5])
+    
+        #for some reason _6 instead of Base_currency
+        #in Cash it changes to _8, because Base_Currency is being used??
+        for security in securities.itertuples():
+            if security.Type == 'STK':
+                stocks['name'].append(security.Ticker)
+                stocks['exchange'].append(security.Exchange)
+                stocks['currency'].append(security._6)
+            elif security.Type == 'CASH':
+                cash['name'].append(security.Ticker)
+                cash['exchange'].append(security.Exchange)
+                cash['symbol'].append(security.Base_Currency)
+                cash['currency'].append(security._8)
+            elif security.Type == 'IND':
+                index['name'].append(security.Ticker)
+                index['exchange'].append(security.Exchange)
+                index['currency'].append(security._6)
+            elif security.Type == 'FUT':
+                cont_futures['name'].append(security.Ticker)
+                cont_futures['exchange'].append(security.Exchange)
+                cont_futures['currency'].append(security._6)
+                cont_futures['contract_id'].append(security.Conid)
             else:
-                conid['name'].append(security[1][0])
-                conid['exchange'].append(security[1][2])
-                conid['contract_id'].append(security[1][8])
+                conid['name'].append(security.Ticker)
+                conid['exchange'].append(security.Exchange)
+                conid['contract_id'].append(security.Conid)
 
         stocks_df = pd.DataFrame.from_dict(stocks)
         index_df = pd.DataFrame.from_dict(index)
@@ -57,6 +67,11 @@ class app_performance:
         return stocks_df, cash_df, index_df, cont_futures_df, contract_ids_df, tickers
 
     def getContractlists(self, filename):
+        '''
+        From the dictionaries created by the getTickerlists() function, we create contract objects that being recognized by the Interactive Brokers API as input for requesting the data. 
+        Since Foreign Exchange trades 24/24 (midweek) they don't have a 'adjusted last' price every closing trading day.
+        That's why they require a different type of request/parameter. 
+        '''
         stocks, cash, index, futures, contract_ids, tickers = self.getTickerlists(filename)
         c_stocks = [ibi.Stock(symbol=name, exchange=exchange, currency=currency) for name, exchange, currency in
                     zip(stocks['name'], stocks['exchange'], stocks['currency'])]
@@ -84,6 +99,9 @@ class app_performance:
         return hist_data
 
     async def getHistoricaldata(self, filename):
+        '''
+        This function connects to the Interactive Brokers API and request historical data in an asynchronous method. This should allow for faster data retrieval. 
+        '''
         self.ib = ibi.IB()
         random_id = random.randint(0, 9999)
         with await self.ib.connectAsync(host='127.0.0.1', port=7496, clientId=random_id):
@@ -101,6 +119,10 @@ class app_performance:
             return all_bars, tickers
 
     def get_df_all_data(self, filename):
+        '''
+        This function creates a dataframe from data that was retrieved by using the getHistoricaldata() function. 
+        In the end we interpolate linearily for missing data. This is an easy way out, and certainly not the best
+        '''
         all_bars_total = []
         all_bars, tickers = asyncio.run(self.getHistoricaldata(filename))
         tickers = list(tickers)
@@ -116,6 +138,9 @@ class app_performance:
         return df.interpolate(), tickers
 
     def get_df_all_data_iex(self, filename):
+        '''
+        This function uses IEX's API for historical data retrieval. Normally this function is not being used, but when there is a bug with Interactive Brokers's API, it's easy way to get going
+        '''
         token = os.environ.get('IEX_TOKEN')
         stocks, cash, index, futures, contract_ids, tickers = self.getTickerlists(filename)
         df = pd.DataFrame()
@@ -125,25 +150,26 @@ class app_performance:
         return df[::-1], tickers
 
     def get_performance(self, data):
-        data = data
-        last_date = data.index[-1]
-        print(last_date)
+        '''
+        This function creates a pandas dataframe. In it, prices changes over different time horizons are being shown in percentage terms
+        '''
         tickerlist = list(data.columns)
         tickerlist = " ".join(tickerlist)
         tickerlist = tickerlist.split()
+        #print(tickerlist[0])
         window_names = ['Ticker', 'Price', '1D', '1W', '3W', '1M', 'MTD', '3M', 'QTD', 'YTD', 'vs 52w max', 'vs 52w min']
         df = pd.DataFrame()
         len_week, len_3w, len_1m, len_mtd, len_3m, len_qtd, len_ytd = self.get_lengths_periods()
         for ticker in tickerlist:
             data_perf = data[ticker]
-            latest = data_perf[-2]
+            latest = data_perf.iloc[-2]
             range = [3, len_week, len_3w, len_1m, len_mtd, len_3m, len_qtd, len_ytd]
             results = []
             for time in range:
-                if data_perf[-time] < latest:
-                    results.append((latest - data_perf[-time]) / latest)
+                if data_perf.iloc[-time] < latest:
+                    results.append((latest - data_perf.iloc[-time]) / latest)
                 else:
-                    results.append(-(data_perf[-time] - latest) / data_perf[-time])
+                    results.append(-(data_perf.iloc[-time] - latest) / data_perf.iloc[-time])
             yearly_high = data_perf.max()
             yearly_low = data_perf.min()
             vs_52_max = -(yearly_high - latest) / yearly_high
@@ -151,13 +177,16 @@ class app_performance:
             results.append(vs_52_max)
             results.append(vs_52_min)
             results = ["{:.2%}".format(y) for y in results]
-            results.insert(0, data_perf[-2].round(2))
+            results.insert(0, data_perf.iloc[-2].round(2))
             df[ticker] = results
         df = df.T.reset_index()
         df.columns = window_names
         return df.sort_values(by = 'Ticker')
 
     def get_correlation_table_window_x(self, df, value):
+        '''
+        This function creates a pandas dataframe. In it, correlations (pearson method) between different contracts are being calculated and shown, over different time horizons.
+        '''
         window_list = [15, 30, 90, 120, 180]
         tickerlist = list(df.columns)
         TICKER = value
@@ -193,6 +222,9 @@ class app_performance:
         return data.round(2), dataframe
 
     def df_rates_spreads(self):
+        '''
+        In this function we make connection to Federal Reserve Economic Data website's API 'fred.stlouisfred.com'. We retrieve data and put it in a pandas dataframe.
+        '''
         names = ['2Y-10Y Spread', '5Y Breakeven', 'HY-OAS', 'IG Spread', 'High Yield', '3M t-bill', '2Y t-note', '5Y t-note', '10Y t-note', '30Y t-note']
         tickers = ['T5YIE', 'BAMLH0A0HYM2', 'BAMLC0A4CBBB', 'BAMLH0A0HYM2EY', 'DTB3', 'DGS2', 'DGS5', 'DGS10', 'DGS30']
         df = pdr.get_data_fred('T10Y2Y')
@@ -202,8 +234,10 @@ class app_performance:
         return df.dropna()
 
     def df_performance_rates_spreads(self):
+        '''
+        By using the function df_rates_spreads() we import relevant historical data. In this function we calculate the changes in percentage of each financial instrument over different time horizons
+        '''
         dataframe = self.df_rates_spreads()
-        # print(dataframe)
         window_names = ['Ticker', 'Price', '1D', '1W', '1M', '3M', 'vs 52w max', 'vs 52w min', 'vs 3Y ave', 'vs 5Y ave']
         df = pd.DataFrame()
         ticker_list = ['2Y-10Y Spread', '5Y Breakeven', 'HY-OAS', 'IG Spread', 'High Yield', '3M t-bill', '2Y t-note', '5Y t-note', '10Y t-note', '30Y t-note']
@@ -245,6 +279,8 @@ class app_performance:
         return df
 
     def chart_rates_spreads(self, data, TICKER):
+        '''
+        This function creates a interactive line chart from the dataframe inputed'''
         x = data.index
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=x, y=np.array(data), name=TICKER))
@@ -261,13 +297,20 @@ class app_performance:
         return fig
 
     def getDaterange(self, numdays):
+        '''
+        Input a certain (integer) number. This function will create a list of dates going x (your input) number of days back from today.
+        '''
         end_date = datetime.datetime.today()
         delta = datetime.timedelta(days=numdays)
         start_date = end_date - delta
         date_list = pd.Series(pd.bdate_range(start_date, end_date))
+        #date_list = pd.bdate_range(start_date, end_date)
         return date_list
 
     def get_yesterday(self):
+        '''
+        Give me yesterday's date. If yesterday was a weekend day, this functions returns the previous friday
+        '''
         today = date.today()
         yesterday = today - timedelta(days=1)
         if today.weekday() == 6:
@@ -276,65 +319,13 @@ class app_performance:
             yesterday = date.today() - timedelta(days=3)
         return yesterday
 
-    def df_performance_rates_spreads(self):
-        window_names = ['Ticker', 'Price', '1D', '1W', '1M', '3M', 'vs 52w max', 'vs 52w min', 'vs 3Y ave', 'vs 5Y ave']
-        dataframe = pd.DataFrame()
-        df = pd.DataFrame()
-        tickers = ['T10Y2Y', 'T5YIE', 'BAMLH0A0HYM2', 'BAMLC0A4CBBB', 'BAMLH0A0HYM2EY', 'DTB3', 'DGS2', 'DGS5', 'DGS10', 'DGS30']
-        names = ['2Y-10Y Spread', '5Y Breakeven', 'HY-OAS', 'IG Spread', 'High Yield', '3M t-bill', '2Y t-note', '5Y t-note', '10Y t-note', '30Y t-note']
-        len_week, len_3w, len_1m, len_mtd, len_3m, len_qtd, len_ytd = self.get_lengths_periods()
-        for ticker, name, in zip(tickers, names):
-            dtf = pdr.get_data_fred(ticker)
-            df = df.join(dtf)
-            print(df)
-            data = df[name]
-            latest = data[-1]
-            range = [2, len_week, len_1m, len_3m]
-            results = []
-            for time in range:
-                if data[-time] < latest:
-                    results.append((latest - data[-time]))
-                else:
-                    results.append(-(data[-time] - latest))
-            yearly_high = data[-252:].max()
-            yearly_low = data[-252:].min()
-            y3_ave = data[-756:].sum()/756
-            y5_ave = data[-1260:].sum()/1260
-            vs_52_max = ((latest - yearly_high) / latest)
-            vs_52_min = ((latest - yearly_low) / latest)
-            vs_y3_ave = ((latest - y3_ave) / latest)
-            vs_y5_ave = ((latest - y5_ave) / latest)
-            results_vs = [vs_52_max, vs_52_min, vs_y3_ave, vs_y5_ave]
-            for i in results_vs:
-                results.append(i)
-            results = ["{0:.1f}".format(y*100) for y in results]
-            results.insert(0, data[-1].round(2))
-            dataframe[name] = results
-        dataframe = df.T.reset_index()
-        dataframe.columns = window_names
-        # print(dataframe)
-        return dataframe
-
-    def chart_rates_spreads(self, data, TICKER):
-        x = data.index
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=x, y=np.array(data), name=TICKER))
-        fig.update_xaxes(title='date')
-        fig.update_yaxes(title='performance')
-        fig.update_layout(
-            title={
-                'text': f"{TICKER} performance",
-                'y': 0.9,
-                'x': 0.5,
-                'xanchor': 'center',
-                'yanchor': 'top'}
-        )
-        return fig
-
     def get_lengths_periods(self):
+        '''
+        This function calculates the amount of days we need to go back from today to retrieve the desired lengths of dataframes (excluding weekenddays) for differents starting dates. 
+        '''
         end_dt = self.get_yesterday()
-        start_dt_ytd = date(2021, 12, 31)
-        start_dt_qtd = date(2022, 6, 30)
+        start_dt_ytd = date(datetime.datetime.now().year -1, 12, 31)
+        start_dt_qtd = pendulum.now('Europe/Paris').first_of('quarter').date()
         start_dt_3m = end_dt - timedelta(weeks=12)
         start_dt_mtd = end_dt.replace(day=1) - timedelta(days=1)
         start_dt_week = end_dt - timedelta(weeks=1)
@@ -366,10 +357,14 @@ class app_performance:
         return len_week, len_3w, len_4w, len_mtd, len_3m, len_qtd, len_ytd
 
     def daterange(self, date1, date2):
+        '''
+        returns a range of dates between two input dates.
+        '''
         for n in range(int((date2 - date1).days) + 1):
             yield date1 + timedelta(n)
 
 
-# ap = app_performance()
-# df = ap.get_df_all_data_iex('tickers/sectors.csv')
-# print(df)
+#ap = app_performance()
+#df = ap.get_df_all_data('tickers_corr/correlations.csv')
+#perf = ap.get_performance(df[0])
+#print(perf)
